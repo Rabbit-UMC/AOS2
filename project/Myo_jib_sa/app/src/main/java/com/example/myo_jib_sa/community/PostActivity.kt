01bd873ecdb00288479d6076f5964c404dd54807
ShimFFF <shimseawon1510@gmail.com>
2023-07-08 22:44:01 +0900

    <feat> : 게시물 레이아웃

---
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/PostActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myo_jib_sa.community
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.myo_jib_sa.databinding.ActivityPostBinding
+
+class PostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPostBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPostBinding.inflate(layoutInflater)
+
+    }
+
+
+}
