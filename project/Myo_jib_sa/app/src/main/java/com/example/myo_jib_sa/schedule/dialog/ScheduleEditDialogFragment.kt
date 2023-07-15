d2f773c7b83a901ef998554726bfda3aca64ed84
hyeonji91 <hjkim1770@gmail.com>
2023-07-15 19:06:12 +0900

    <fix>: 일정 제목 글자 수 제한

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/schedule/dialog/ScheduleEditDialogFragment.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/schedule/dialog/ScheduleEditDialogFragment.kt
@@ -75,10 +75,17 @@ class ScheduleEditDialogFragment : DialogFragment() {
     5. 정규식 패턴 ^[ ] or ^[\\s] : 공백 허용
      **/
     private val editTextFilter = InputFilter { source, start, end, dest, dstart, dend ->
-        val ps = Pattern.compile("[ㄱ-ㅎㅏ-ㅣ가-힣a-z-A-Z0-9()&_\\s-]+$")
-        if (!ps.matcher(source).matches()) {
+        val ps = Pattern.compile("[ㄱ-ㅎㅏ-ㅣ가-힣a-z-A-Z0-9()&_\\s-]+")
+        val input = dest.subSequence(0, dstart).toString() + source.subSequence(start, end) + dest.subSequence(dend, dest.length).toString()
+        val matcher = ps.matcher(input)
+
+        // 글자수 제한 설정 (예: 최대 10자)
+        val maxLength = 10
+        if (matcher.matches() && input.length <= maxLength) {
+            source
+        } else {
             ""
-        } else source
+        }
     }
 
     private fun setSpinnerDialog(position:Int){
