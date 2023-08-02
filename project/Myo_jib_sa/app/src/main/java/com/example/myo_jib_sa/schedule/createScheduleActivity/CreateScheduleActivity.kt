e7c70deb2d4338986742a10c7fca4ecbd3dc7bb1
hyeonji91 <hjkim1770@gmail.com>
2023-08-03 04:15:04 +0900

    <feat>: 키보드 adjustResize추가

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/schedule/createScheduleActivity/CreateScheduleActivity.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/schedule/createScheduleActivity/CreateScheduleActivity.kt
@@ -3,10 +3,16 @@ package com.example.myo_jib_sa.schedule.createScheduleActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myo_jib_sa.R
+import com.example.myo_jib_sa.databinding.ActivityCreateScheduleBinding
+import com.example.myo_jib_sa.databinding.ActivityCurrentMissionBinding
 
 class CreateScheduleActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCreateScheduleBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_schedule)
+        binding = ActivityCreateScheduleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
