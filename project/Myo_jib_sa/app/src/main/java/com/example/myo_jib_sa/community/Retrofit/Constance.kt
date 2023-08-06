a98a6e9656e8a035ef8639a82642bc317cceeb02
ShimFFF <shimseawon1510@gmail.com>
2023-08-06 17:05:24 +0900

    <Refactor> : 비슷한 기능 통합     <fix> : 관리자 페이지 사진 수정 레이아웃 수정

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/Retrofit/Constance.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/Retrofit/Constance.kt
@@ -7,10 +7,10 @@ object Constance {
     const val ART_ID:Int=3 //예술 게시판 id
     const val EXERCISE_ID:Int=2 //운동 게시판 id
     const val FREE_ID:Int=1 //자유 게시판 ID
-
-   const val author="Authorization"
+    const val author="Authorization"
 
     //유저 id 13인 토큰
-    val jwt:String= BuildConfig.KAKAO_API_KEY
+    const val jwt:String= BuildConfig.API_KEY
     const val USER_ID:Long=13
 }
+
