16257021bbf42e28a86838656757e548ad3f7949
Dongwon Shin <99808693+edv-Shin@users.noreply.github.com>
2023-12-04 04:04:54 +0900

    Revert "Develop mission main"

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/schedule/currentMissionActivity/adapter/CurrentMissionCurrentMissionAdapter.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/schedule/currentMissionActivity/adapter/CurrentMissionCurrentMissionAdapter.kt
@@ -18,20 +18,6 @@ class CurrentMissionCurrentMissionAdapter (private val missionList:ArrayList<Cur
                                            private val width:Int, private val height:Int):
     RecyclerView.Adapter<CurrentMissionCurrentMissionAdapter.ViewHolder>() {
 
-    //클릭 이벤트 처리 ==============================================
-//리스너 인터페이스
-    interface OnItemClickListener{
-        fun onClick(currentMissionData: CurrentMissionResult)
-        fun onLongClick(position: Int)
-        fun onScheduleClick(currentMissionData: CurrentMissionResult)
-    }
-     //(3) 외부에서 클릭 시 이벤트 설정
-     fun setItemClickListener(onItemClickListener: OnItemClickListener) {
-         this.itemClickListener = onItemClickListener
-     }
-    // (4) setItemClickListener로 설정한 함수 실행
-    lateinit private var itemClickListener : OnItemClickListener
-    //==============================================================
 
     //화면 설정
     override fun onCreateViewHolder(
@@ -40,7 +26,7 @@ class CurrentMissionCurrentMissionAdapter (private val missionList:ArrayList<Cur
     ): ViewHolder {
         val binding =
             ItemCurrentMissionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        //binding.root.layoutParams = ConstraintLayout.LayoutParams((width*0.46).toInt(), (width*0.46*1.2).toInt()) todo
+        binding.root.layoutParams = ConstraintLayout.LayoutParams((width*0.46).toInt(), (width*0.46*1.2).toInt())
         //binding.frameLayout.layoutParams = ConstraintLayout.LayoutParams((width*0.40).toInt(), (height*0.16).toInt())
         return ViewHolder(binding)
     }
@@ -61,31 +47,37 @@ class CurrentMissionCurrentMissionAdapter (private val missionList:ArrayList<Cur
             itemClickListener.onLongClick(position)
             true // 반드시 true를 반환해야 합니다.
         }
-
-        //작성한 일지 클릭 이벤트
     }
 
-    inner class ViewHolder(private val binding: ItemCurrentMissionBinding) :
+    class ViewHolder(private val binding: ItemCurrentMissionBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
         fun bind(data: CurrentMissionResult) {
-            //todo
-            //binding.missionImg.clipToOutline = true //이미지 모서리 둥글게
+
+            binding.missionImg.clipToOutline = true //이미지 모서리 둥글게
             binding.missionTitleTv.text = data.missionTitle
             binding.missionDdayTv.text = data.dday
-            binding.missionChallengerTv.text = "${data.challengerCnt}"
-
-            binding.missionScheduleTv.setOnClickListener {
-                itemClickListener.onScheduleClick(data)
-            }
-                //todo
-//            Glide.with(itemView)
-//                .load(data.image)
-//                .error(R.drawable.ic_currentmission_free) //에러시 보여줄 이미지
-//                .fallback(R.drawable.ic_currentmission_free) //load할 url이 비어있을 경우 보여줄 이미지
-//                .into(binding.missionImg)//이미지 설정
+            binding.missionChallengerTv.text = "${data.challengerCnt}명"
+            Glide.with(itemView)
+                .load(data.image)
+                .error(R.drawable.ic_currentmission_free) //에러시 보여줄 이미지
+                .fallback(R.drawable.ic_currentmission_free) //load할 url이 비어있을 경우 보여줄 이미지
+                .into(binding.missionImg)//이미지 설정
         }
     }
+
+    //클릭 이벤트 처리 ==============================================
+    //리스너 인터페이스
+    interface OnItemClickListener{
+        fun onClick(currentMissionData: CurrentMissionResult)
+        fun onLongClick(position: Int)
+    }
+    // (3) 외부에서 클릭 시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+    // (4) setItemClickListener로 설정한 함수 실행
+    private lateinit var itemClickListener : OnItemClickListener
+    //==============================================================
 
 
 
@@ -93,5 +85,3 @@ class CurrentMissionCurrentMissionAdapter (private val missionList:ArrayList<Cur
         return missionList.size
     }
 }
-
-
