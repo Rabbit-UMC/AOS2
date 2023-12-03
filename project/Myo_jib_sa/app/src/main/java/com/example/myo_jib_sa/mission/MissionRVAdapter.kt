5203c3ae45c3a6e1c0e58ab1f604a9a97e0ba8c8
Dongwon Shin <edvedv@naver.com>
2023-12-04 03:59:00 +0900

    <feat> 미션 메인 api 연결, 탭 레이아웃과 카테고리 필터링, 신고 스와이프, 신고 다이얼로그 레이아웃

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mission/MissionRVAdapter.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mission/MissionRVAdapter.kt
@@ -1,96 +1,65 @@
 package com.example.myo_jib_sa.mission
 
-import android.content.Context
-import android.graphics.Rect
 import android.util.Log
 import android.view.*
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.myo_jib_sa.databinding.ItemMissionMissionBinding
-
-sealed class MissionItem {
-    abstract val missionId: Long
-    abstract val title: String
-    abstract val challengerCnt: Int
-    abstract val startAt: String
-    abstract val endAt: String
-    abstract val content: String
-    abstract val categoryId: Long
-    abstract val image: String
-}
-
-data class Home(
-    override val missionId: Long,
-    override val title: String,
-    override val challengerCnt: Int,
-    override val startAt: String,
-    override val endAt: String,
-    override val content: String,
-    override val categoryId: Long,
-    override val image: String
-) : MissionItem()
-
-data class Category(
-    override val missionId: Long,
-    override val title: String,
-    override val challengerCnt: Int,
-    override val startAt: String,
-    override val endAt: String,
-    override val content: String,
-    override val categoryId: Long,
-    override val image: String
-) : MissionItem()
-
+import com.example.myo_jib_sa.mission.API.Mission
 
 class MissionRVAdapter(
-    private val context: Context,
-    private var dataList: List<MissionItem>,
+    private var dataList: List<Mission>,
     private val onItemClickListener: OnItemClickListener,
-    private val onItemLongClickListener: OnItemLongClickListener
-) : RecyclerView.Adapter<MissionRVAdapter.ViewHolder>() {
+    private val onReportClickListener: OnReportClickListener
+) : RecyclerView.Adapter<MissionRVAdapter.MissionViewHolder>() {
 
-    interface OnItemLongClickListener {
-        fun onItemLongClick(item:MissionItem)
+    interface OnReportClickListener {
+        fun onReportClick(item:Mission)
     }
-
     interface OnItemClickListener {
-        fun onItemClick(item:MissionItem)
+        fun onItemClick(item:Mission)
     }
 
-    inner class ViewHolder(private val binding: ItemMissionMissionBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: MissionItem) {
+    inner class MissionViewHolder(private val binding: ItemMissionMissionBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Mission) {
+            with(binding) {
+                Glide.with(missionImgIv)
+                    .load(item.image)
+                    .into(missionImgIv)
+                missionTitleTxt.text = item.title
+                missionCntTxt.text = item.challengerCnt.toString()
+                missionDateTxt.text = "D+60"
 
-            binding.missionTitleTxt.text = item.title
-            binding.missionCntTxt.text = item.challengerCnt.toString()
-            binding.missionDateTxt.text = "D+60"
+                Log.d("home",item.missionId.toString())
 
-            Log.d("home",item.missionId.toString())
+                // 신고 클릭 이벤트 처리
+                missionReportTxt.setOnClickListener {
+                    //클릭된 아이템의 위치 가져오기
+                    val itemPosition = dataList[adapterPosition]
+                    onReportClickListener.onReportClick(itemPosition)
+                    Log.d("home","report ID: {$itemPosition.id.toString()}")
+                    true // 이벤트가 소비되었음을 반환
+                }
 
-            // 롱클릭 이벤트 처리(신고)
-            binding.root.setOnLongClickListener {
-                //클릭된 아이템의 위치 가져오기
-                val itemPosition = dataList[adapterPosition]
-                onItemLongClickListener.onItemLongClick(itemPosition)
-                Log.d("home","report ID: {$itemPosition.id.toString()}")
-                true // 이벤트가 소비되었음을 반환
-            }
-
-            // 간단한 클릭 이벤트 처리(미션 상세보기)
-            binding.root.setOnClickListener {
-                val itemPosition = dataList[adapterPosition]
-                onItemClickListener.onItemClick(itemPosition)
-                Log.d("home","detail ID: {$itemPosition.id.toString()}")
+                // 클릭 이벤트 처리(미션 상세보기)
+                root.setOnClickListener {
+                    val itemPosition = dataList[adapterPosition]
+                    onItemClickListener.onItemClick(itemPosition)
+                    Log.d("home","detail ID: {$itemPosition.id.toString()}")
+                }
             }
         }
+
     }
 
     // 뷰홀더를 생성하여 반환
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MissionViewHolder {
         val binding = ItemMissionMissionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        return MissionViewHolder(binding)
     }
 
     //뷰홀더 데이터 설정
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MissionViewHolder, position: Int) {
         val item = dataList[position]
         holder.bind(item)
     }
@@ -100,20 +69,12 @@ class MissionRVAdapter(
         return dataList.size
     }
 
-    //간격 설정을 위한 클래스
-    class CustomItemDecoration(private val spaceHeight: Int) : RecyclerView.ItemDecoration() {
-        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-            outRect.top = spaceHeight // 아이템 사이의 간격을 설정 (위 넓이)
-            outRect.bottom=spaceHeight //아래
+    fun updateDataByCategory(wholeList:List<Mission>, newCategoryId: Int) {
+        dataList = if (newCategoryId != 0){
+            wholeList.filter { it.categoryId == newCategoryId.toLong() }
+        } else {
+            wholeList
         }
-    }
-    // 아이템 간격 설정을 위한 메소드
-    fun setItemSpacing(recyclerView: RecyclerView, spacing: Int) {
-        recyclerView.addItemDecoration(CustomItemDecoration(spacing))
-    }
-
-    fun updateData(/*newItems: List<MissionItem>*/) {
-        //dataList = newItems
         notifyDataSetChanged()
     }
 }
