5b3afc264c23ca9665d8195a7a94453b8bfa8daa
Dongwon Shin <edvedv@naver.com>
2023-12-19 01:30:05 +0900

    <feat> 마이페이지 레이아웃 구현

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/MyPagePostRVAdapter.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/MyPagePostRVAdapter.kt
@@ -6,31 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myo_jib_sa.databinding.ItemMyPostBinding
-import com.example.myo_jib_sa.mypage.API.Post
+import com.example.myo_jib_sa.databinding.ItemMyPagePostBinding
+import com.example.myo_jib_sa.mypage.api.GetMyPostResult
 
-class MyPostAdapter(
+class MyPagePostRVAdapter(
     private val context: Context,
-    private val dataList: List<Post>,
-) : RecyclerView.Adapter<MyPostAdapter.ViewHolder>() {
+    private val dataList: List<GetMyPostResult>,
+) : RecyclerView.Adapter<MyPagePostRVAdapter.ViewHolder>() {
 
 
     inner class ViewHolder(
-        private val binding: ItemMyPostBinding
+        private val binding: ItemMyPagePostBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: Post) {
+        fun bind(item: GetMyPostResult) {
             // 다른 바인딩 작업도 추가
 
             //시간 날짜 포멧
             val formattedTime = item.uploadTime.substring(11, 16)
             val formattedDate = item.uploadTime.substring(5, 7) + "/" + item.uploadTime.substring(8, 10)
 
-            binding.postItemNameTxt.text=item.articleTitle
-            binding.postItemUploadTimeTxt.text=formattedTime
-            binding.postItemUploadDateTxt.text=formattedDate
-            binding.postItmeHeartNumTxt.text=item.likeCount.toString()
-            binding.postItmeCommentCntTxt.text=item.commentCount.toString()
+            binding.myPageTabPostTitleTxt.text = item.articleTitle
+            binding.myPageTabLikeCntTxt.text = item.likeCount.toString()
+            binding.myPageTabCommentCntTxt.text = item.commentCount.toString()
         }
     }
 
@@ -38,7 +36,7 @@ class MyPostAdapter(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        val binding = ItemMyPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemMyPagePostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
