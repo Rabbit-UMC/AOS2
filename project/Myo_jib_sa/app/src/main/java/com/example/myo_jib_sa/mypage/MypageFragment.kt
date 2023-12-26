a8f2ce17105b3cf88b7a6e965f6021f080ed297f
Dongwon Shin <edvedv@naver.com>
2023-12-27 00:43:09 +0900

    <fix> 디렉토리명 변경으로 인한 import 디버깅

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/MypageFragment.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/MypageFragment.kt
@@ -12,11 +12,9 @@ import com.bumptech.glide.Glide
 import com.example.myo_jib_sa.R
 import com.example.myo_jib_sa.base.MyojibsaApplication.Companion.sRetrofit
 import com.example.myo_jib_sa.databinding.FragmentMypageBinding
-import com.example.myo_jib_sa.mypage.adapter.MypageViewPagerAdapter
 import com.example.myo_jib_sa.mypage.api.GetUserProfileResponse
-/*import com.example.myo_jib_sa.mission.MissionAdapter
-import com.example.myo_jib_sa.mission.MissionItem*/
 import com.example.myo_jib_sa.mypage.api.MypageAPI
+import com.example.myo_jib_sa.mypage.adapter.MypageViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import retrofit2.Call
 import retrofit2.Callback
