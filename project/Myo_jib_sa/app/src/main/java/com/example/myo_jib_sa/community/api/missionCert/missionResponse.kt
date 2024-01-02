2498f149882d5e781298312001d8ef8f1b1159ce
ShimFFF <shimseawon1510@gmail.com>
2024-01-02 20:34:03 +0900

    :sparkles: Feat : 미션 인증 뷰페이저 완성 (좋아요 처리 X)

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/api/missionCert/missionResponse.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/api/missionCert/missionResponse.kt
@@ -1,19 +1,18 @@
 package com.example.myo_jib_sa.community.api.missionCert
 
+import com.example.myo_jib_sa.base.BaseResponse
+
 data class MissionResponse(
-    val isSuccess:String,
-    val code:Int,
-    val message:String,
     val result: MissionResult
-)
+): BaseResponse()
 data class MissionResult(
     val mainMissionId:Long,
     val mainMissionName:String,
-    val dday:String,
-    val mainMissionContent:String,
     val startDay:String,
+    val mainMissionContent:String,
     val rank:List<Rank>,
-    val missionProofImages:List<MissionProofImages>
+    val missionProofImages:List<MissionProofImages>,
+    val dday:String,
 )
 data class Rank(
     val userId:Long,
