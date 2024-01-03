56a1a713c4d6f6804b3927576a6280773bde4ba3
ShimFFF <shimseawon1510@gmail.com>
2024-01-03 23:34:55 +0900

    :truck: Rename : missionCert로 이동

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/missionCert/ActivityMissionPicture.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/missionCert/ActivityMissionPicture.kt
@@ -1,12 +1,12 @@
-package com.example.myo_jib_sa.community
+package com.example.myo_jib_sa.community.missionCert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myo_jib_sa.R
 
-class MissionCertificationWriteCheckActivity : AppCompatActivity() {
+class ActivityMissionPicture : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mission_certification_write_check)
+        setContentView(R.layout.activity_mission_picture)
     }
 }
--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/missionCert/MissionCertificationWriteCheckActivity.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/missionCert/MissionCertificationWriteCheckActivity.kt
@@ -1,12 +1,12 @@
-package com.example.myo_jib_sa.community
+package com.example.myo_jib_sa.community.missionCert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myo_jib_sa.R
 
-class ActivityMissionPicture : AppCompatActivity() {
+class MissionCertificationWriteCheckActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mission_picture)
+        setContentView(R.layout.activity_mission_certification_write_check)
     }
 }
