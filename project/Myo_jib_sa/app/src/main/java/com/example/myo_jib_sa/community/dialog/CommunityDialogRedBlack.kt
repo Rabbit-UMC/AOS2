ab80a1a511bd5a66e10cb970efcd2cbb29611332
ShimFFF <shimseawon1510@gmail.com>
2024-01-07 23:28:59 +0900

    :sparkles: Feat : 커스텀 dialog, redBlack, blueWhite 추가

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/dialog/CommunityDialogRedBlack.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/dialog/CommunityDialogRedBlack.kt
@@ -3,19 +3,20 @@ package com.example.myo_jib_sa.community.dialog
 import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Outline
 import android.os.Bundle
+import android.view.View
+import android.view.ViewOutlineProvider
 import android.view.WindowManager
 import android.widget.Button
-import com.example.myo_jib_sa.databinding.DialogCommunityOkBinding
+import com.example.myo_jib_sa.databinding.DialogCommunityRedBlackBinding
 
-class CommunityDialog(context: Context
-                      , private val title:String
-                      , private val detail:String
-                      , private val okTxt:String
-                      , private val cancleTxt:String
-                      , private val backgroundColor:String
-                      , private val okColor:String) : Dialog(context) {
-    private lateinit var binding: DialogCommunityOkBinding
+class CommunityDialogRedBlack(context: Context
+                              , private val title:String
+                              , private val detail:String
+                              , private val okTxt:String
+                              , private val cancleTxt:String) : Dialog(context) {
+    private lateinit var binding: DialogCommunityRedBlackBinding
 
 
     interface CustomDialogListener {
@@ -31,17 +32,23 @@ class CommunityDialog(context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= DialogCommunityOkBinding.inflate(layoutInflater)
+        binding= DialogCommunityRedBlackBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.communityDialogConstraint.clipToOutline=true
+        binding.communityDialogConstraint.clipToOutline = true
+        binding.communityDialogConstraint.outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View?, outline: Outline?) {
+                // radius: 8dp
+                outline?.setRoundRect(0, 0, view?.width ?: 0, view?.height ?: 0, convertDpToPixel(8f, view?.context ?: return))
+
+            }
+        }
+
 
         // 다이얼로그가 포커스를 가지도록 설정
         window?.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
         window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
 
-        val cancelButton: Button = binding.communityPopupCancelBtn
-        val okButton: Button = binding.communityPopupOkBtn
 
         //text 설정
         binding.communityPopupTxt.text= title
@@ -49,23 +56,23 @@ class CommunityDialog(context: Context
         binding.communityPopupOkBtn.text=okTxt
         binding.communityPopupNoBtn.text=cancleTxt
 
-        //색상 설정
-        binding.communityDialogConstraint.setBackgroundColor(Color.parseColor(backgroundColor))
-        binding.communityPopupOkBtn.setBackgroundColor(Color.parseColor(okColor))
 
         // 취소 버튼 클릭 리스너 설정
-        cancelButton.setOnClickListener {
+        binding.communityPopupNoBtn.setOnClickListener {
             // 다이얼로그 닫기
             dismiss()
         }
 
         // 확인 버튼 클릭 리스너 설정
-        okButton.setOnClickListener {
+        binding.communityPopupOkBtn.setOnClickListener {
             // 확인 버튼 클릭 이벤트 처리
             listener?.onPositiveButtonClicked(true)
             // 다이얼로그 닫기
             dismiss()
         }
     }
-
+    // dp를 픽셀로 변환하는 메서드
+    private fun convertDpToPixel(dp: Float, context: Context): Float {
+        return dp * context.resources.displayMetrics.density
+    }
 }
