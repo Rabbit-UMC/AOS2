0d7e60ed0a9814e508eb7d8dd0a88c712f5ac306
Dongwon Shin <edvedv@naver.com>
2024-01-24 21:50:18 +0900

    <feat> 마이페이지 히스토리 api 연결

--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/history/MypageHistoryFailFragment.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/history/MypageHistoryFailFragment.kt
@@ -1,16 +1,23 @@
 package com.example.myo_jib_sa.mypage.history
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myo_jib_sa.base.MyojibsaApplication
 import com.example.myo_jib_sa.databinding.FragmentMypageHistoryFailBinding
 import com.example.myo_jib_sa.databinding.FragmentMypageHistorySuccessBinding
 import com.example.myo_jib_sa.mypage.adapter.MypageHistoryRVAdapter
 import com.example.myo_jib_sa.mypage.api.GetHistoryResponse
+import com.example.myo_jib_sa.mypage.api.MypageAPI
+import com.example.myo_jib_sa.mypage.api.UserMissionResDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MypageHistoryFailFragment : Fragment() {
+class MypageHistoryFailFragment(private val nickname: String?) : Fragment() {
     lateinit var binding: FragmentMypageHistoryFailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,11 +25,39 @@ class MypageHistoryFailFragment : Fragment() {
     ): View? {
         binding = FragmentMypageHistoryFailBinding.inflate(layoutInflater)
 
-        val myItems: List<GetHistoryResponse> = arrayListOf(GetHistoryResponse("1"), GetHistoryResponse("1"), GetHistoryResponse("1"))
-        val adapter = MypageHistoryRVAdapter(myItems)
-        binding.mypageHistoryFailVp.adapter = adapter
+        binding.mypageHistoryFailNicknameTv.text = nickname
+
+        getFailureHistory()
 
         return binding.root
     }
 
+    private fun getFailureHistory() {
+        MyojibsaApplication.sRetrofit.create(MypageAPI::class.java)
+            .getFailureHistory()
+            .enqueue(object : Callback<GetHistoryResponse> {
+                override fun onResponse(
+                    call: Call<GetHistoryResponse>,
+                    response: Response<GetHistoryResponse>
+                ) {
+                    val historyResponse = response.body()
+                    if(historyResponse != null && historyResponse.isSuccess){
+                        setViewpager(historyResponse.result.userMissionResDtos)
+                        binding.mypageHistoryFailCntTv.text = historyResponse.result.targetCnt.toString()+"개"
+                        binding.mypageHistoryFailWholeMissionTv.text = historyResponse.result.missionCnt.toString()+"개"
+                        binding.mypageHistoryFailMissionFailCntTv.text = historyResponse.result.targetCnt.toString()+"개"
+                    }
+                }
+
+                override fun onFailure(call: Call<GetHistoryResponse>, t: Throwable) {
+                    Log.d("getFailureHistory", "onFailure : $t")
+                }
+
+            })
+    }
+
+    private fun setViewpager(items : List<UserMissionResDto>) {
+        val adapter = MypageHistoryRVAdapter(items)
+        binding.mypageHistoryFailVp.adapter = adapter
+    }
 }
--- project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/history/MypageHistorySuccessFragment.kt
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/mypage/history/MypageHistorySuccessFragment.kt
@@ -1,16 +1,22 @@
 package com.example.myo_jib_sa.mypage.history
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.myo_jib_sa.community.api.manager.ManagerMissionJoinRequest
+import com.example.myo_jib_sa.base.MyojibsaApplication.Companion.sRetrofit
 import com.example.myo_jib_sa.databinding.FragmentMypageHistorySuccessBinding
 import com.example.myo_jib_sa.mypage.adapter.MypageHistoryRVAdapter
 import com.example.myo_jib_sa.mypage.api.GetHistoryResponse
+import com.example.myo_jib_sa.mypage.api.MypageAPI
+import com.example.myo_jib_sa.mypage.api.UserMissionResDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MypageHistorySuccessFragment : Fragment() {
+class MypageHistorySuccessFragment(private val nickname: String?) : Fragment() {
     lateinit var binding: FragmentMypageHistorySuccessBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,11 +24,38 @@ class MypageHistorySuccessFragment : Fragment() {
     ): View? {
         binding = FragmentMypageHistorySuccessBinding.inflate(layoutInflater)
 
-        val myItems: List<GetHistoryResponse> = arrayListOf(GetHistoryResponse("1"), GetHistoryResponse("1"), GetHistoryResponse("1"))
-        val adapter = MypageHistoryRVAdapter(myItems)
-        binding.mypageHistorySuccessVp.adapter = adapter
+        binding.mypageHistorySuccessNicknameTv.text = nickname
+        getSuccessHistory()
 
         return binding.root
     }
 
+    private fun getSuccessHistory() {
+        sRetrofit.create(MypageAPI::class.java)
+            .getSuccessHistory()
+            .enqueue(object : Callback<GetHistoryResponse> {
+                override fun onResponse(
+                    call: Call<GetHistoryResponse>,
+                    response: Response<GetHistoryResponse>
+                ) {
+                    val historyResponse = response.body()
+                    if(historyResponse != null && historyResponse.isSuccess){
+                        setViewpager(historyResponse.result.userMissionResDtos)
+                        binding.mypageHistorySuccessCntTv.text = historyResponse.result.targetCnt.toString()+"개"
+                        binding.mypageHistorySuccessWholeMissionTv.text = historyResponse.result.missionCnt.toString()+"개"
+                        binding.mypageHistorySuccessMissionSuccessCntTv.text = historyResponse.result.targetCnt.toString()+"개"
+                    }
+                }
+
+                override fun onFailure(call: Call<GetHistoryResponse>, t: Throwable) {
+                    Log.d("getSuccessHistory", "onFailure : $t")
+                }
+
+            })
+    }
+
+    private fun setViewpager(items : List<UserMissionResDto>) {
+        val adapter = MypageHistoryRVAdapter(items)
+        binding.mypageHistorySuccessVp.adapter = adapter
+    }
 }
