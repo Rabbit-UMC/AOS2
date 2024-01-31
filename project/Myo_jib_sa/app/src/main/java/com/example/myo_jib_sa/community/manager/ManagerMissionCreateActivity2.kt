8d8255c8bdcdcac1229a0ac7b6cf7c63915bb116
ShimFFF <shimseawon1510@gmail.com>
2024-02-01 01:57:38 +0900

    :sparkles: Feat : 미션 공양 관리자 뷰 설정

---
+++ project/Myo_jib_sa/app/src/main/java/com/example/myo_jib_sa/community/manager/ManagerMissionCreateActivity2.kt
@@ -0,0 +1,227 @@
+package com.example.myo_jib_sa.community.manager
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.DialogFragment
+import com.example.myo_jib_sa.R
+import com.example.myo_jib_sa.base.MyojibsaApplication
+import com.example.myo_jib_sa.databinding.ActivityMissionCreateBinding
+import com.example.myo_jib_sa.databinding.ToastMissionCreateBinding
+import com.example.myo_jib_sa.mission.api.MissionAPI
+import com.example.myo_jib_sa.mission.api.MissionCategoryListResponse
+import com.example.myo_jib_sa.mission.api.MissionCategoryListResult
+import com.example.myo_jib_sa.mission.api.MissionCreateRequests
+import com.example.myo_jib_sa.mission.api.MissionCreateResponse
+import com.example.myo_jib_sa.mission.dialog.MissionCreateCalendarDialogFragment
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class ManagerMissionCreateActivity2 : AppCompatActivity(), MissionCreateCalendarDialogFragment.OnDateSelectedListener {
+    private lateinit var binding: ActivityMissionCreateBinding
+
+    private lateinit var referenceDate : LocalDate //오늘 날짜
+    private lateinit var selectedDate : LocalDate //종료 날짜
+
+    private lateinit var startSelectedDate : LocalDate //시작 날짜
+    private lateinit var endSelectedDate : LocalDate //종료 날짜
+
+    private var isStartDateSelected = false
+    private var isEndDateSelected = false
+    private var isMissionTitleInputted = false
+    private var isMissionMemoInputted = false
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMissionCreateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //관리자 전용 뷰 세팅
+        setView()
+
+        //오늘 날짜
+        referenceDate = LocalDate.now()
+        selectedDate = referenceDate
+
+        startSelectedDate = referenceDate
+        endSelectedDate = referenceDate
+
+        binding.missionCreateStartDateBtnTxt.text = fromDateYYYYMMDD(startSelectedDate)
+        binding.missionCreateEndDateBtnTxt.text = fromDateYYYYMMDD(endSelectedDate)
+
+
+        initListener()
+
+    }
+
+    //관리자 전용 뷰 세팅
+    private fun setView(){
+        binding.missionCreateCategoryRadioGroup.visibility= View.GONE
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(binding.missionCreateInfoCl)
+
+        constraintSet.connect(
+            binding.missionCreateTitleEt.id,
+            ConstraintSet.TOP,
+            com.example.myo_jib_sa.R.id.mission_create_info_desc_txt,
+            ConstraintSet.BOTTOM,
+            20
+        )
+
+        constraintSet.applyTo(binding.missionCreateInfoCl)
+
+        binding.titleTv.text="미션 공양"
+        binding.missionCreateInfoDescTxt.text="달토끼에게 공양하는 미션입니다. 신중히 적어주세요."
+        binding.missionCreateMemoOpenTxt.text="인수인계 활성화"
+
+    }
+
+
+    //미션 생성 API 연결
+    private fun postMissionCreate() {
+        var missionRequest : MissionCreateRequests
+        // Retrofit을 사용한 API 호출
+        with(binding) {
+            missionRequest = MissionCreateRequests(
+                title = missionCreateTitleEt.text.toString(),
+                startAt = missionCreateStartDateBtnTxt.text.toString(),
+                endAt = missionCreateEndDateBtnTxt.text.toString(),
+                categoryId = missionCreateCategoryRadioGroup.checkedRadioButtonId.toLong(),
+                isOpen = if(missionCreateOpenSwitch.isChecked) 0 else 1,
+                content = missionCreateMemoEt.text.toString(),
+                status = "ACTIVE"
+            )
+            Log.d("postMissionCreate", "missionRequest : $missionRequest")
+        }
+
+        MyojibsaApplication.sRetrofit.create(MissionAPI::class.java).postMissionCreate(missionRequest).enqueue(object :
+            Callback<MissionCreateResponse> {
+            override fun onResponse(call: Call<MissionCreateResponse>, response: Response<MissionCreateResponse>) {
+                val writeResponse = response.body()
+                if (writeResponse != null) {
+                    if(writeResponse.isSuccess){
+                        showSnackbar(writeResponse.errorMessage)
+                        finish()
+                    }
+                    else {
+                        showSnackbar(writeResponse.errorMessage)
+                    }
+
+                }
+            }
+            override fun onFailure(call: Call<MissionCreateResponse>, t: Throwable) {
+                showSnackbar("네트워크 요청 실패")
+            }
+        })
+
+    }
+
+    // 리스너 초기화
+    private fun initListener(){
+        with(binding) {
+            missionCreateTitleEt.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun afterTextChanged(p0: Editable?) {}
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    isMissionTitleInputted = missionCreateTitleEt.text.isNotEmpty()
+                    setCreateButtonIsEnabled()
+                }
+            })
+            missionCreateMemoEt.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun afterTextChanged(p0: Editable?) {}
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    missionCreateMemoCntTxt.text = missionCreateMemoEt.text.length.toString()
+                    isMissionMemoInputted = missionCreateMemoEt.text.isNotEmpty()
+                    setCreateButtonIsEnabled()
+                }
+            })
+
+            // 시작일
+            missionCreateStartDateBtnTxt.setOnClickListener {
+                showCalendarDialog(true)
+            }
+
+            // 종료일
+            missionCreateEndDateBtnTxt.setOnClickListener {
+                showCalendarDialog(false)
+            }
+
+            // 완료 버튼
+            missionCreateCompleteBtn.setOnClickListener {
+                postMissionCreate()
+            }
+
+            // 뒤로가기 버튼
+            missionBackBtn.setOnClickListener {
+                finish()
+            }
+        }
+    }
+
+    // Flag 값에 따라 Create 버튼 enabled 값 설정
+    private fun setCreateButtonIsEnabled() {
+        binding.missionCreateCompleteBtn.isEnabled =
+            isMissionTitleInputted && isMissionMemoInputted
+    }
+
+    private fun showCalendarDialog(isStartDate: Boolean) {
+        val calendarDialog = MissionCreateCalendarDialogFragment(isStartDate)
+        calendarDialog.setStyle(DialogFragment.STYLE_NORMAL, R.style.roundCornerBottomSheetDialogTheme)
+        calendarDialog.setDateSelectedListener(this)
+        calendarDialog.show(supportFragmentManager, "MissionCreateCalendarDialogFragment")
+    }
+
+    private fun showSnackbar(message: String) {
+        val snackbarBinding = ToastMissionCreateBinding.inflate(layoutInflater)
+        snackbarBinding.toastMissionReportTxt.text = message
+
+        val snackbar = Snackbar.make(binding.root, "", Snackbar.LENGTH_SHORT).apply {
+            animationMode = BaseTransientBottomBar.ANIMATION_MODE_FADE
+            (view as Snackbar.SnackbarLayout).apply {
+                setBackgroundColor(Color.TRANSPARENT)
+                addView(snackbarBinding.root)
+                translationY = -70.dpToPx().toFloat()
+                elevation = 0f
+            }
+        }
+
+        snackbar.show()
+    }
+
+    override fun onStartDateSelected(date: String) {
+        binding.missionCreateStartDateBtnTxt.text = date
+    }
+
+    override fun onEndDateSelected(date: String) {
+        binding.missionCreateEndDateBtnTxt.text = date
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun fromDateYYYYMMDD(date: LocalDate?): String? {
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        return date?.format(formatter)
+    }
+
+    private fun Int.dpToPx(): Int = (this * resources.displayMetrics.density).toInt()
+}
